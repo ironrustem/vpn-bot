2660d6d3d7f2bf84ded7d530a7e118c88d33526f
Rustem <rsayfullin@it-one.ru>
2023-04-29 13:00:36 +0300

    [create user]

---
+++ vpn_bot/main.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import os
+import sys
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Привет, я бот Маркиз!👋 Я даю доступ к впн)💻 \n\nНу что, готов получить доступ к лучшему сервису ?)")
+
+@dp.message(Command("Get"))
+async def cmd_start(message: types.Message):
+    os.system('./wireguard-install.sh')
+    os.system('1')
+    os.system(f"{message.from_user.id}")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    bot = Bot(token=sys.argv[1])
+    asyncio.run(main())
