469320292311e029a7e901d3c2744e5d6e72de31
Rustem <rsayfullin@it-one.ru>
2023-04-29 15:08:19 +0300

    11

--- vpn_bot/main.py
+++ vpn_bot/main.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import os
 import sys
+from aiogram.types import FSInputFile
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -20,6 +21,8 @@ async def cmd_start(message: types.Message):
 @dp.message(Command("Get"))
 async def cmd_start(message: types.Message):
     print(os.system(f"./wireguard-install.sh {message.from_user.id}"))
+    agenda = FSInputFile("/root/", filename=f"wg0-client-{message.from_user.id}.conf")
+    await message.reply_document(agenda)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
